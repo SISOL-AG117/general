4fddee31f85eb768c8128d6535bb58f59b982fd9
Daniel Mendoza <200021245+miauriachi@users.noreply.github.com>
2026-06-10 23:28:17 -0600

    Publish AR route for branch-based Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/general/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.source.html',
+    },
+  },
 })
